c3889425768371d9e4f390c1787063b49b120df6
Gub2007 <ghs654123@gmail.com>
2025-02-11 10:47:57 -0300

    11/02

---
+++ index.php
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+<head>
+
+</head>
+<body>
+
+   <div class="container-fluid bg-primary text-white text-center p-5">
+        <h1>Projeto SW2</h1>
+        <p>
+            Elaborado por Guilerme Hnerique de Souza
+</p>
+        </div>
+<div class="container">
+    <div class="row">
+
+    <div class="col-sm4">
+    <h1> Administrador</h1>
+   </div>
+
+   <div class="col-sm4">
+   <h1> Catwgorias</h1>
+    </div>
+    
+    <div class="col-sm4">
+    <h1> Produtos</h1>
+   </div>
+   
+   
+</body>
+</html>

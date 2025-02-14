973d1a3880b7a6401cf3d07c027679a0c76f2a55
Gub2007 <ghs654123@gmail.com>
2025-02-14 11:41:08 -0300

    14/02

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-
-
-
-
-
-
+</head>
+<body>
 
 <head>
 
@@ -16,24 +20,38 @@
    <div class="container-fluid bg-primary text-white text-center p-5">
         <h1>Projeto SW2</h1>
         <p>
-            Elaborado por Guilerme Hnerique de Souza
+            Elaborado por Gustavo Henrique 
 </p>
         </div>
 <div class="container">
     <div class="row">
 
     <div class="col-sm4">
-    <h1> Administrador</h1>
+    <h1 class="bg-seccess text-warning"> Administrador</h1>
+    <p class="text-danger"> 
+   Utilize esta opção para realizar o cadastro de novos administradires, ou edita-los.
+</p>
    </div>
 
    <div class="col-sm4">
-   <h1> Catwgorias</h1>
-    </div>
+   <h1  class="bg-danger"> Categorias</h1>
+   <p class="text-success">
+    Utilize esta opção para cadstrar os fornecedores 
+    dos produtos que serão vendidos on-line.
+</div>
     
     <div class="col-sm4">
-    <h1> Produtos</h1>
+    <h1  class="bg-primary"> Produtos</h1>
    </div>
    
-   
+   <div class="col-sm-6">
+        <h1>Produtos</h1>
+    <div>
+
+    <div class="col=sm-6">
+        <h1>Vendas</h1>
+   </div>
+
+
 </body>
 </html>
